a50c0745fc848b39f5949290d9292f71d61443d0
rafaelmodesto <rafaelmodesto09@gmail.com>
2020-03-28 13:38:53 -0300

    Mobile da aplicação

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -48,7 +48,7 @@ export default function Register() {
 
           <Link className="back-link" to="/">
            <FiArrowLeft size={16} color="E02041" />
-            Não tenho cadastro
+            Voltar para logon
           </Link>
         </section>
 
